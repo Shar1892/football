5d4764e8e2a6ec074636acfbefd186fbffd3058e
sg.sharov <sg.sharov@tensor.ru>
2022-01-15 10:26:13 +0300

    fix: исправил условия вывода данных в попапе

--- src/components/MatchPopup/MatchPopup.js
+++ src/components/MatchPopup/MatchPopup.js
@@ -16,35 +16,23 @@ const MatchPopup = ({match, isOpen, popupClose}) => {
 			className={`popup__overlay ${isOpen ? 'popup__overlay_visible' : ''}`}
 			onClick={handleClose}
 		>
-			<div className='popup'>
-				<p className='popup__tour-number'>
-					{`Tour ${match.tourNumber ? match.tourNumber : ''}`}
-				</p>
-				<div className='popup__competitor-container'>
-					<p className='popup__competitor-name'>
-						{match.teamHome ? match.teamHome.name : ''}
-					</p>
-					<p className='popup__competitor-score'>
-						{match.teamHome ? match.scoreFtHome : ''}
-					</p>
+			{match.tourNumber && (
+				<div className='popup'>
+					<p className='popup__tour-number'>{'Tour ' + match.tourNumber}</p>
+					<div className='popup__competitor-container'>
+						<p className='popup__competitor-name'>{match.teamHome.name}</p>
+						<p className='popup__competitor-score'>{match.scoreFtHome}</p>
+					</div>
+					<div className='popup__competitor-container'>
+						<p className='popup__competitor-name'>{match.teamAway.name}</p>
+						<p className='popup__competitor-score'>{match.scoreFtAway}</p>
+					</div>
+					<p className='popup__date'>{'Date - ' + createDate(match.date)}</p>
+					{match.stadium && (
+						<p className='popup__stadium'>{'Stadium ' + match.stadium.name}</p>
+					)}
 				</div>
-				<div className='popup__competitor-container'>
-					<p className='popup__competitor-name'>
-						{match.teamAway ? match.teamAway.name : ''}
-					</p>
-					<p className='popup__competitor-score'>
-						{match.teamAway ? match.scoreFtAway : ''}
-					</p>
-				</div>
-				<p className='popup__date'>
-					{'Date - ' + createDate(match.date ? match.date : '')}
-				</p>
-				{match.stadium && (
-					<p className='popup__stadium'>
-						{`Stadium - ${match.stadium ? match.stadium.name : ''}`}
-					</p>
-				)}
-			</div>
+			)}
 		</div>
 	);
 };
